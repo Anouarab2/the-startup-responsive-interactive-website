763ceb99a9500842f2a098231f88fc1eefb788dc
Anouarab2 <anouar.ait.brahim@hva.nl>
2025-01-22 12:26:39 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,25 @@
 // selecteer de elementen die aangesproken moeten worden
 let bewaren = document.querySelector('.bewaar-deel')
+let tekst = document.querySelector('.bewaar-deel')
 
 bewaren.addEventListener('click', bewaarHuis)
+// tekst.addEventListener('click', tekstAnders)
+
+tekst.addEventListener('click', function() {
+    tekst.textContent = (tekst.textContent === 'Bewaar') ? 'Bewaard!' : 'Bewaar';
+  });
+
 
 function bewaarHuis(){
     bewaren.classList.toggle('active')
 }
 
+// function tekstAnders(){
+//     tekst.textContent = 'Bewaard';
+// }
+
 bewaren.addEventListener('animationend', bewaarHuis)
+// verander.addEventListener('animationend', tekstTerug )
 
 
 
@@ -18,4 +30,15 @@ scale.addEventListener('click', videoGroter)
 
 function videoGroter(){
     scale.classList.toggle('groter')
+}
+
+
+const openMenu = document.querySelector('.menu-button');
+const menu = document.querySelector('.huis-opties');
+
+openMenu.addEventListener('click', menuOpen)
+
+function menuOpen(){
+    openMenu.classList.toggle('move')
+    menu.classList.toggle('move')
 }
